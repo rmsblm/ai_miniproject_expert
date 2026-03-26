e3b8f45f61e0d8e7fe4a61b589afb7f22e0dfc21
Adoum <adoumrym@gmail.com>
2026-03-26 22:12:45 +0000

    inference Engine

--- inferenceEngine.py
+++ inferenceEngine.py
@@ -1,42 +1,104 @@
 from experta import *
-from knowledge_base import all_movies
+from knowledgeBase import movies
 
 
 class MovieFact(Fact):
-    pass #works as the container
+    pass  # works as the container
+
 
 class MovieEngine(KnowledgeEngine):  # all the rules go here
-    def __init__(self):
+    def __init__(self, min_dur, max_dur, min_year, max_year):
         super().__init__()
-        self.results = [] 
+        self.results = []
+        self.min_dur = min_dur
+        self.max_dur = max_dur
+        self.min_year = min_year
+        self.max_year = max_year
 
- #Action genre rules
+    # Action genre rules
 
-    @Rule(MovieFact(genre="Action", film_type="Feature Film", duration=P(lambda d: d <= 150), year=P(lambda y: y >= 2000)))
-    def Action_feature_film(self):
-          
-      for m in all_movies:
-         if m["genre"] == "Action" and m["type"] == "Feature Film":
-             self.results.append(m)
+    @Rule(MovieFact(genre="Action", film_type="Feature Film"))
+    def action_feature_film(self):
+        for m in movies:
+            if m["genre"] == "Action" and m["type"] == "Feature Film":
+                if m["duration"] >= self.min_dur and m["duration"] <= self.max_dur:
+                    if m["year"] >= self.min_year and m["year"] <= self.max_year:
+                        self.results.append(m)
 
-    @Rule(MovieFact(genre="Action", film_type="Animated Film", duration=P(lambda d: d <= 150), year=P(lambda y: y >= 2000)))
+    @Rule(MovieFact(genre="Action", film_type="Animated Film"))
     def action_animated_film(self):
-          
-      for m in all_movies:
-         if m["genre"] == "Action" and m["type"] == "Animated Film":
-             self.results.append(m)
+        for m in movies:
+            if m["genre"] == "Action" and m["type"] == "Animated Film":
+                if m["duration"] >= self.min_dur and m["duration"] <= self.max_dur:
+                    if m["year"] >= self.min_year and m["year"] <= self.max_year:
+                        self.results.append(m)
 
-    @Rule(MovieFact(genre="Action", film_type="TV Movie", duration=P(lambda d: d <= 150), year=P(lambda y: y >= 2000)))
+    @Rule(MovieFact(genre="Action", film_type="TV Movie"))
     def action_tv_movie(self):
-          
-      for m in all_movies:
-         if m["genre"] == "Action" and m["type"] == "TV Movie":
-             self.results.append(m)
+        for m in movies:
+            if m["genre"] == "Action" and m["type"] == "TV Movie":
+                if m["duration"] >= self.min_dur and m["duration"] <= self.max_dur:
+                    if m["year"] >= self.min_year and m["year"] <= self.max_year:
+                        self.results.append(m)
 
-    @Rule(MovieFact(genre="Action", film_type="Documentary", duration=P(lambda d: d <= 150), year=P(lambda y: y >= 2000)))
+    @Rule(MovieFact(genre="Action", film_type="Documentary"))
     def action_documentary(self):
-          
-      for m in all_movies:
-         if m["genre"] == "Action" and m["type"] == "Documentary":
-             self.results.append(m)
+        for m in movies:
+            if m["genre"] == "Action" and m["type"] == "Documentary":
+                if m["duration"] >= self.min_dur and m["duration"] <= self.max_dur:
+                    if m["year"] >= self.min_year and m["year"] <= self.max_year:
+                        self.results.append(m)
 
+  # Drama genre rules
+
+    @Rule(MovieFact(genre="Drama", film_type="Feature Film"))
+    def drama_feature_film(self):
+        for m in movies:
+            if m["genre"] == "Drama" and m["type"] == "Feature Film":
+                if m["duration"] >= self.min_dur and m["duration"] <= self.max_dur:
+                    if m["year"] >= self.min_year and m["year"] <= self.max_year:
+                        self.results.append(m)
+
+    @Rule(MovieFact(genre="Drama", film_type="TV Movie"))
+    def drama_tv_movie(self):
+        for m in movies:
+            if m["genre"] == "Drama" and m["type"] == "TV Movie":
+                if m["duration"] >= self.min_dur and m["duration"] <= self.max_dur:
+                    if m["year"] >= self.min_year and m["year"] <= self.max_year:
+                        self.results.append(m)
+
+    # Comedy genre rules
+
+    @Rule(MovieFact(genre="Comedy", film_type="Feature Film"))
+    def comedy_feature_film(self):
+        for m in movies:
+            if m["genre"] == "Comedy" and m["type"] == "Feature Film":
+                if m["duration"] >= self.min_dur and m["duration"] <= self.max_dur:
+                    if m["year"] >= self.min_year and m["year"] <= self.max_year:
+                        self.results.append(m)
+
+    @Rule(MovieFact(genre="Comedy", film_type="Animated Film"))
+    def comedy_animated_film(self):
+        for m in movies:
+            if m["genre"] == "Comedy" and m["type"] == "Animated Film":
+                if m["duration"] >= self.min_dur and m["duration"] <= self.max_dur:
+                    if m["year"] >= self.min_year and m["year"] <= self.max_year:
+                        self.results.append(m)
+
+    # History genre rules
+
+    @Rule(MovieFact(genre="History", film_type="Feature Film"))
+    def history_feature_film(self):
+        for m in movies:
+            if m["genre"] == "History" and m["type"] == "Feature Film":
+                if m["duration"] >= self.min_dur and m["duration"] <= self.max_dur:
+                    if m["year"] >= self.min_year and m["year"] <= self.max_year:
+                        self.results.append(m)
+
+    @Rule(MovieFact(genre="History", film_type="Documentary"))
+    def history_documentary(self):
+        for m in movies:
+            if m["genre"] == "History" and m["type"] == "Documentary":
+                if m["duration"] >= self.min_dur and m["duration"] <= self.max_dur:
+                    if m["year"] >= self.min_year and m["year"] <= self.max_year:
+                        self.results.append(m)
